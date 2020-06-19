f61b4d1bf8cfa73198fb48f44643dd34379fb809
RaphX <59814852+RaphXz@users.noreply.github.com>
2020-06-20 05:55:44 +0700

    Update url.js

--- url.js
+++ url.js
@@ -1,10 +1,5 @@
 const urls = [
-    "http://aurora-discord.glitch.me/ping",
-    "http://ecobot-inex.glitch.me",
-    "http://pikabot-discord.glitch.me",
-    "http://giveawayboat-discord.glitch.me",
-    "http://snowboats.glitch.me",
-    "http://elixirbot.glitch.me"
+      "https://kaori-js.glitch.me"
 ];
 
 module.exports = urls;
