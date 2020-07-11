8175ed026e2fc5da6404335f3b7c5aa65df67796
Raphxz <59814852+RaphxTech@users.noreply.github.com>
2020-07-11 20:04:04 +0700

    Update url.js

--- url.js
+++ url.js
@@ -1,7 +1,7 @@
 const urls = [
       "https://kaori-js.glitch.me",
       "https://editor-js.glitch.me",
-      "https://rrljs-bot.glitch.me"
+      "https://raph-rllbot.glitch.me"
 ];
 
 module.exports = urls;
