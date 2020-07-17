450f014f2566b9f67d16b830f5cde8c6e8a5f733
RaphTz <59814852+raphtz@users.noreply.github.com>
2020-07-18 02:02:17 +0700

    Update url.js

--- url.js
+++ url.js
@@ -1,9 +1,8 @@
 const urls = [
-      "https://kaori-js.glitch.me",
       "https://editor-js.glitch.me",
       "https://raph-rllbot.glitch.me",
-"https://moosik-js.glitch.me",
-"https://discord-temp-channel.glitch.me"
+      "https://moosik-js.glitch.me",
+      "https://discord-temp-channel.glitch.me"
 ];
 
 module.exports = urls;
