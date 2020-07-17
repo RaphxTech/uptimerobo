7d8787e10f8fd3d08b1def57a82038a6213acd85
RaphTz <59814852+raphtz@users.noreply.github.com>
2020-07-18 02:32:34 +0700

    Update url.js

--- url.js
+++ url.js
@@ -1,7 +1,7 @@
 const urls = [
       "https://editor-js.glitch.me",
       "https://raph-rllbot.glitch.me",
-      "https://moosik-js.glitch.me",
+      "https://moosik-bot.glitch.me",
       "https://discord-temp-channel.glitch.me"
 ];
 
