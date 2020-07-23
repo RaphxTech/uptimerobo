9cb1cc3348a4777c6ad700680bbd654a0238101b
RaphTz <59814852+raphtz@users.noreply.github.com>
2020-07-24 01:32:27 +0700

    Update url.js

--- url.js
+++ url.js
@@ -2,7 +2,8 @@ const urls = [
       "https://editor-js.glitch.me",
       "https://raph-rllbot.glitch.me",
       "https://moosikjs.glitch.me",
-      "https://discord-temp-channel.glitch.me"
+      "https://discord-temp-channel.glitch.me",
+      "https://kaoriproject.glitch.me"
 ];
 
 module.exports = urls;
